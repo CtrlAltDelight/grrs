966682fdf77b7587b370acf89eaf858eb8a88e90
CtrlAltDelight <lbchigges@gmail.com>
2023-12-14 17:15:48 -0500

    Created an error message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
@@ -6,15 +7,18 @@ struct Cli {
     path: std::path::PathBuf, // file to read from
 }
 
-fn main() {
+fn main() -> Result<()> {
     // Get the command line arguments
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
